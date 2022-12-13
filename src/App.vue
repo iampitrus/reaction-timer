20c465a28a5461e2fd8ce04654985cfea3f13f85
Ogbonnaya Peter <ogbonnapete210@gmail.com>
2022-12-13 04:16:50 +0100

    reaction added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
+<!-- The program randomly delays the loading of the block to check how fast you are to click it anytime it appears on your screen. -->
+
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p v-if="showResult">Reaction time: {{ score }} ms</p>
 </template>
 
 <script>
@@ -13,12 +16,20 @@ export default {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
+      showResult: false,
     };
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
+      this.showResult = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
     },
   },
 };
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,12 +1,40 @@
 <template>
-  <div class="block">
-    click me
-  </div>
+  <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
 </template>
 
 <script>
 export default {
+  // We add props to our project like this
   props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  // mounted is called when the component is mounted to the dom
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  // updated is called when any update takes place in the component
+  updated() {},
+  // this fires when this component is unmounted from the dom
+  unmounted() {},
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit("end", this.reactionTime);
+    },
+  },
 };
 </script>
 
